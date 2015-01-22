c21dad0a25f203b5cb4c213932fb8b790c228c70
Mieszko Stelmach <mieszko.stelmach@stxnext.pl>
2015-01-22 09:49:54 +0100

    Game Center is default now. Changes in layout.

--- grot/src/main/java/pl/stxnext/grot/activity/MenuActivity.java
+++ grot/src/main/java/pl/stxnext/grot/activity/MenuActivity.java
@@ -1,22 +1,31 @@
 package pl.stxnext.grot.activity;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesActivityResultCodes;
+import com.google.example.games.basegameutils.BaseGameActivity;
+
 import pl.stxnext.grot.R;
+import pl.stxnext.grot.config.AppConfig;
 
 /**
  * Created by Tomasz Konieczny on 2015-01-07.
  */
-public class MenuActivity extends Activity {
+public class MenuActivity extends BaseGameActivity {
 
     public static final String START_SCREEN_ARG = "is_start_screen";
 
     public static final int RESTART_GAME = 1421;
 
+    private static final int REQUEST_LEADERBOARD = 1546;
+    private static final int REQUEST_ACHIEVEMENTS = 1236;
+
     public boolean isStartScreen;
+    public int userRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +71,8 @@ public class MenuActivity extends Activity {
         findViewById(R.id.game_center_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, GameServicesActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MenuActivity.this, GameServicesActivity.class);
+//                startActivity(intent);
             }
         });
 
@@ -75,5 +84,78 @@ public class MenuActivity extends Activity {
             }
         });
 
+        findViewById(R.id.leaderboard_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MenuActivity.this.userRequest = REQUEST_LEADERBOARD;
+                if (isSignedIn()) {
+                    showLeaderboard();
+                } else {
+                    beginUserInitiatedSignIn();
+                }
+            }
+        });
+
+        findViewById(R.id.achievements_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MenuActivity.this.userRequest = REQUEST_ACHIEVEMENTS;
+                if (isSignedIn()) {
+                    showAchievements();
+                } else {
+                    beginUserInitiatedSignIn();
+                }
+            }
+        });
+
+        SharedPreferences preferences = getSharedPreferences(AppConfig.SHARED_PREFS, MODE_PRIVATE);
+        if (preferences.getBoolean(AppConfig.GOOGLE_PLAY_GAMES_STATUS, true)) {
+            beginUserInitiatedSignIn();
+        }
+
+    }
+
+    @Override
+    public void onSignInFailed() {
+        SharedPreferences preferences = getSharedPreferences(AppConfig.SHARED_PREFS, MODE_PRIVATE);
+        preferences.edit().putBoolean(AppConfig.GOOGLE_PLAY_GAMES_STATUS, false).apply();
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        SharedPreferences preferences = getSharedPreferences(AppConfig.SHARED_PREFS, MODE_PRIVATE);
+        preferences.edit().putBoolean(AppConfig.GOOGLE_PLAY_GAMES_STATUS, true).apply();
+        switch (userRequest) {
+            case REQUEST_LEADERBOARD:
+                showLeaderboard();
+                break;
+            case REQUEST_ACHIEVEMENTS:
+                showAchievements();
+                break;
+        }
+    }
+
+    private void showLeaderboard() {
+        Intent intent = Games.Leaderboards.getLeaderboardIntent(getApiClient(), getString(R.string.leaderboard_id));
+        startActivityForResult(intent, REQUEST_LEADERBOARD);
+    }
+
+    private void showAchievements() {
+        Intent intent = Games.Achievements.getAchievementsIntent(getApiClient());
+        startActivityForResult(intent, REQUEST_ACHIEVEMENTS);
+    }
+
+    @Override
+    protected void onActivityResult(int request, int response, Intent data) {
+        super.onActivityResult(request, response, data);
+        if (request == REQUEST_LEADERBOARD || request == REQUEST_ACHIEVEMENTS) {
+            if (response == GamesActivityResultCodes.RESULT_RECONNECT_REQUIRED) {
+                if (isSignedIn()) {
+                    reconnectClient();
+                } else {
+                    beginUserInitiatedSignIn();
+                }
+            }
+        }
     }
 }
